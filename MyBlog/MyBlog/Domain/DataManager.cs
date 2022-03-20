56833c1bf89ea0f7b9f2f919618028f4ffd0ead2
GreenAcidy <47273458+GreenAcidy@users.noreply.github.com>
2022-03-21 02:51:54 +0300

    add migrations, connect database

---
+++ MyBlog/MyBlog/Domain/DataManager.cs
@@ -0,0 +1,17 @@
+﻿using MyBlog.Domain.Repositories.Abstract;
+
+namespace MyBlog.Domain
+{
+    public class DataManager
+    {
+        public ITextFieldsRepository TextFields { get; set; }
+        public IServiceItemsRepository Items { get; set; }
+        public DataManager (ITextFieldsRepository textFieldsRepository, IServiceItemsRepository serviceItemsRepository)
+        {
+            TextFields = textFieldsRepository;
+            Items=serviceItemsRepository;
+        }
+
+
+    }
+}
--- MyBlog/MyBlog/Startup.cs
+++ MyBlog/MyBlog/Startup.cs
@@ -1,15 +1,19 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using MyBlog.Service;
+using MyBlog.Domain.Repositories.EntityFramework;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MyBlog.Domain.Repositories.Abstract;
+using MyBlog.Domain;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 
 namespace MyBlog
 {
@@ -22,12 +26,50 @@ namespace MyBlog
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //connect configuration from appsetting.json
             Configuration.Bind("Project", new Config());
+
+            //connect the necessary functionality
+            services.AddTransient<ITextFieldsRepository, EFTextFieldsRepository>();
+            services.AddTransient<IServiceItemsRepository, EFServiceItemsRepository>();
+            services.AddTransient<DataManager>();
+
+            //connect context DB
+            services.AddDbContext<AppDbContext>(x => x.UseSqlServer(Config.ConnectionString));
+
+            //setting up the system
+            services.AddIdentity<IdentityUser, IdentityRole>(opts =>
+            {
+                opts.User.RequireUniqueEmail = true;
+                opts.Password.RequiredLength = 6;
+                opts.Password.RequireNonAlphanumeric = false;
+                opts.Password.RequireLowercase = false;
+                opts.Password.RequireUppercase = false;
+                opts.Password.RequireDigit = false;
+            }).AddEntityFrameworkStores<AppDbContext>().AddDefaultTokenProviders();
+
+            //setting up authentication cookie
+            services.ConfigureApplicationCookie(options =>
+            {
+                options.Cookie.Name = "myBlogAuth";
+                options.Cookie.HttpOnly = true;
+                options.LoginPath = "/account/login";
+                options.AccessDeniedPath = "/account/accessdenied";
+                options.SlidingExpiration = true;
+            });
+
+            //setting up the authorization policy for Admin area
+            services.AddAuthorization(x =>
+            {
+                x.AddPolicy("AdminArea", policy => { policy.RequireRole("admin"); });
+            });
+
+            //add services for views and controllers
             services.AddControllersWithViews().SetCompatibilityVersion(CompatibilityVersion.Version_3_0).AddSessionStateTempDataProvider(); 
         }
+
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -47,8 +89,12 @@ namespace MyBlog
 
             app.UseRouting();
 
+            //connect system of authentication and authorization
+            app.UseCookiePolicy();
+            app.UseAuthentication();
             app.UseAuthorization();
 
+            //register endpoints we need
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
